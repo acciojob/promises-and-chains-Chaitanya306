f052670f2dba9e6badaadadc5989cbdf5b5ef143
Chaitanya306 <98307330+Chaitanya306@users.noreply.github.com>
2025-03-31 11:13:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+let age=document.getElementById('age')
+let name=document.getElementById('name')
+function myFunction(){
+	
+	if(age.value=='' || name.value==''){
+		alert("Please enter valid details.")
+		return
+	}
+	//alert('in the function')
+	let promise=new Promise(function(res,rej){
+		if(age.value>18){ 
+			setTimeout(()=>res("Welcome, . You can vote."),4000)
+		}	
+		else { 
+			setTimeout(()=>rej("Oh sorry . You aren't old enough."),4000)
+			 }
+	})
+	promise.then(e=>alert(e))
+	.catch(e=>alert(e))
+}
+/*
+function pro(){
+	//alert('in the function')
+	return new Promise((res,rej)=>{
+		if(age.value>18){ 
+			setTimeout(()=>res("Welcome, . You can vote."),4000)
+		}	
+		else { 
+			setTimeout(()=>rej("Oh sorry . You aren't old enough."),4000)
+			 }
+	})
+}
+*/
